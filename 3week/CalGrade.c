4ca3e50c65a2138f7e281db018d95b4ff4e08232
JinjooSeo <jin.joo.seo@cern.cj>
2021-01-27 00:10:10 +0900

    Add comment

--- 3week/CalGrade.c
+++ 3week/CalGrade.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//define scores and grades
 const int NUM_OF_ARG = 9;
 const int SCORES[NUM_OF_ARG] = { 95, 90, 85, 80, 75, 70, 65, 60, 0 };
 const char* GRADES[NUM_OF_ARG] = { "A+", "A", "B+", "B", "C+", "C", "D+", "D", "F" };
@@ -9,25 +10,26 @@ int setScore();
 const char* getGrade(int score);
 
 int main(int argc, char *argv[]) {
-  printf("---------------------- 학점 프로그램 ----------------------\n");
-  printf("---------------- 종료를 원하면 ""999"" 를 입력 ----------------\n");
+  printf("학점 프로그램\n");
+  printf("종료를 원하면 ""999"" 를 입력\n");
 
-  printGradeTable();
+  printGradeTable(); //print grade table
 
-  int score = -1;
-  score = setScore();
+  int score = -1; //intialize the score
+  score = setScore(); //set score from external input
 
-  while(score!=999){
+  while(score!=999){ //print out the grade until score equals to 999
     if (score >100 || score < 0) printf("** %d 성적을 올바르게 입력하세요. 범위는 0 ~ 100입니다.\n", score);
-    else printf("학점은 %s 입니다.\n", getGrade(score));
-    score = setScore();
+    else printf("학점은 %s 입니다.\n", getGrade(score)); //assgin the grade by score and print it
+    score = setScore();  //get grade by score
   }
 
-  printf("학점 프로그램을 종료합니다.\n");
+  printf("학점 프로그램을 종료합니다.\n"); //EOS
 
   return 0;
 }
 
+// TO DO..? : print grades and scores within table
 void printGradeTable(){
   printf("[ 학점 테이블 ]\n");
 
@@ -44,6 +46,7 @@ void printGradeTable(){
 
 }
 
+//set score from external input
 int setScore(){
   int input;
 
@@ -53,6 +56,7 @@ int setScore(){
   return input;
 }
 
+//assgin the grade by score
 const char* getGrade(int score){
   if (score >= 95) return GRADES[0];
   else if (score >= 90 && score < 95) return GRADES[1];
